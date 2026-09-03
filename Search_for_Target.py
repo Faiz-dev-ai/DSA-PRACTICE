0aaa3d2099998045612b2337d9226d7d50be7536
Faiz-zaman <faizzamanpasha@gmail.com>
2026-09-03 21:47:44 +0530

    Add files via upload

---
+++ Search_for_Target.py
@@ -0,0 +1,14 @@
+def searchElement(nums,target):
+    low = 0 
+    high = len(nums)-1
+    while(low <= high):
+        mid = (low+high)//2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid]<target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+nums = [1,2,4,5,6,8,9]
+print(searchElement(nums,8))
